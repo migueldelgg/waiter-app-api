e27a2392c234c1003ccbff71544749b72804b9ae
Miguel Delgado <delgdevoleper@gmail.com>
2025-05-03 16:33:21 -0300

    feat: create product ready

--- src/app/useCases/products/createProduct.ts
+++ src/app/useCases/products/createProduct.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from "express";
+import { Product } from "../../models/Product";
 
-export async function createProduct (req: Request, res: Response) {
+export async function createProduct(req: Request, res: Response) {
   try {
-    console.log(req.body);
+    const { name, description, price, category, ingredients } = req.body;
+
+    const product = await Product.create({
+      description,
+      name,
+      imagePath: req.file?.filename,
+      price: Number(price),
+      ingredients: JSON.parse(ingredients),
+      category,
+    });
+
+    res.status(201).json({ product });
+
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
